c9025e1c62f9bf32017db0ab667417d3272a8e70
Leong <stapler_1@hotmail.com>
2021-07-25 10:20:17 +0800

    Encapsulated some data

--- script.js
+++ script.js
@@ -1,19 +1,51 @@
 const gameboard = (() => {
-    let arr = ['o','o','o','x','x','x','x','x','x'];
+    let arr = ['','','','','','','','',''];
+    let currentPlayer = 1;
 
-    return arr;
+    function displayBoard(){
+        const items = document.querySelectorAll('.grid-items');
+        for(let i = 0; i < arr.length; i++){
+            items[i].innerHTML = arr[i].toUpperCase();
+        }
+    }
+    const grids = document.querySelectorAll('.grids');
+    grids.forEach(grid => grid.addEventListener('click', setBoard));
+
+    function setBoard(e){
+        let gridIndex = this.querySelector('.grid-items').getAttribute('grid') - 1;
+        if(arr[gridIndex] === ''){
+            if(currentPlayer%2 + 1 === 1){
+                arr[gridIndex] = 'o';
+                currentPlayer++;
+            }
+            else if(currentPlayer%2 + 1 === 2){
+                arr[gridIndex] = 'x';
+                currentPlayer++;
+            }
+            else console.log('ERROR');
+            
+            displayBoard();
+        }
+    }
+
+    return {displayBoard};
 })();
 
 function player(name){
-
-    return {name};
-}
-
-function setBoard(){
-    const items = document.querySelectorAll('.grid-items');
-    for(let i = 0; i < gameboard.length; i++){
-        items[i].innerHTML = gameboard[i].toUpperCase();
+    if( typeof player.counter == 'undefined' ) {
+        player.counter = 0;
     }
+
+    const playerNumber = ++player.counter;
+
+    function getPlayerNumber(){
+        return playerNumber;
+    }
+
+    return {name, getPlayerNumber};
 }
 
-setBoard();
+gameboard.displayBoard();
+
+const jeff = player('jeff');
+const arun = player('arun');
