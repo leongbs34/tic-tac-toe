307cb6645ba35f552dd404fa49ed2e365bd8b2bc
Leong <stapler_1@hotmail.com>
2021-08-04 21:29:39 +0800

    Completed

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const gameboard = (() => {
     let arr = ['','','','','','','','',''];
     let currentPlayer = 0;
+    let winningText = '';
 
     function displayBoard(){
         const items = document.querySelectorAll('.grid-items');
@@ -68,49 +69,76 @@ const gameboard = (() => {
     function checkWin(player){
         const playerNumber = player.getPlayerNumber();
         const joinedArr = arr.join('');
+        const gameover = document.querySelector('.gameover')
     
         if(playerNumber === 1){ // o
             if(checkRows('o') || checkCols('o') || checkDiagonals('o')){
-                console.log(`${player.name} wins!`)
+                winningText = `${player.name} wins!`;
+                gameover.textContent = winningText;
                 return true;
             }
         }
     
         else if(playerNumber === 2){ // x
             if(checkRows('x') || checkCols('x') || checkDiagonals('x')){
-                console.log(`${player.name} wins!`)
+                winningText = `${player.name} wins!`;
+                gameover.textContent = winningText;
                 return true;
             }
         }
 
         if(joinedArr.length == 9){
-            console.log('Draw');
+            winningText = 'Draw';
+            gameover.textContent = winningText;
             return true;
         }
+
+        return false;
     }
 
     const grids = document.querySelectorAll('.grids');
     grids.forEach(grid => grid.addEventListener('click', setBoard));
 
     function setBoard(e){
+        let gameFinished = false;
         let gridIndex = this.querySelector('.grid-items').getAttribute('grid') - 1;
         if(arr[gridIndex] === ''){
             if(currentPlayer%2 + 1 === 1){
                 arr[gridIndex] = 'o';
                 displayBoard();
-                checkWin(player1);
+                gameFinished = checkWin(player1);
                 currentPlayer++;
             }
             else if(currentPlayer%2 + 1 === 2){
                 arr[gridIndex] = 'x';
                 displayBoard();
-                checkWin(player2);
+                gameFinished = checkWin(player2);
                 currentPlayer++;
             }
-            else console.log('ERROR');
+            if(gameFinished){
+                document.querySelector('#blur-bg').classList.remove('hide');
+                document.querySelector('#main').classList.add('blur');
+            }
         
         }
     }
+    
+    function resetBoard(){
+        arr = ['','','','','','','','',''];
+        currentPlayer = 0;
+        displayBoard();
+        gameover = '';
+    }
+
+    function removeBlur(){
+        document.querySelector('#blur-bg').classList.add('hide');
+        document.querySelector('#main').classList.remove('blur');
+        resetBoard();
+    }
+
+    const blurBg = document.querySelector('#blur-bg');
+    blurBg.addEventListener('click', removeBlur)
+
 
     return {displayBoard};
 })();
@@ -131,5 +159,5 @@ function player(name){
 
 gameboard.displayBoard();
 
-const player1 = player('player1');
-const player2 = player('player2');
+const player1 = player('O');
+const player2 = player('X');
